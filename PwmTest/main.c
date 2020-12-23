848837c0f3f09eb5b7f35177c50df6f396156a93
Jorge Miranda <mirandazaratejorge@gmail.com>
2020-12-23 11:41:41 -0500

    Add .gitignore file and create an AtmelStudio C project for ATmega328P.

---
+++ PwmTest/main.c
@@ -0,0 +1,18 @@
+/*
+ * PwmTest.c
+ *
+ * Created: 12/23/2020 11:36:15 AM
+ * Author : Jorge Miranda
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
